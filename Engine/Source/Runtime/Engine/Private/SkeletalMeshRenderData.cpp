6c3b14faf1e56722d093f0294569bc3acc348216
jian ru <Jian.Ru@epicgames.com>
2019-09-19 11:59:09 -0400

    Back out changelist 8858830. It can cause incorrect skeletal mesh data being put in to DDC. [FYI] Alexis Matte
    [CL 8889454 by jian ru in Main branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMeshRenderData.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMeshRenderData.cpp
@@ -32,7 +32,7 @@ namespace SkeletalMeshCookStats
 // differences, etc.) replace the version GUID below with a new one.
 // In case of merge conflicts with DDC versions, you MUST generate a new GUID
 // and set this new GUID as the version.                                       
-#define SKELETALMESH_DERIVEDDATA_VER TEXT("70D1F1C048AF4DF2AF870B51CE803A66")
+#define SKELETALMESH_DERIVEDDATA_VER TEXT("4F54F64FD82B40C7A9F4158AF684B64D")
 
 static const FString& GetSkeletalMeshDerivedDataVersion()
 {
